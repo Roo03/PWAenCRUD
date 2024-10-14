eb2f08439e73ba6746f348e354e298fc1ae9c70d
Roo03 <144763464+Roo03@users.noreply.github.com>
2024-10-14 00:11:18 -0600

    fix commit

--- service_worker.js
+++ service_worker.js
@@ -8,7 +8,7 @@ const urlsToCache = [
   '/icon-512x512.png'
 ];
 
-// Instalando el Service Worker y almacenando recursos en caché
+
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
@@ -19,22 +19,22 @@ self.addEventListener('install', event => {
   );
 });
 
-// Interceptando peticiones y respondiendo desde la caché
+
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        // Si encontramos el recurso en la caché, lo retornamos
+        
         if (response) {
           return response;
         }
-        // Si no está en la caché, lo solicitamos a la red
+        
         return fetch(event.request);
       })
   );
 });
 
-// Actualizando el Service Worker
+
 self.addEventListener('activate', event => {
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
